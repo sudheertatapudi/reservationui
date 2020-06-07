02a09e1bfb0a5ca5ddbd29fd2528c9625671cc7e
sudheertatapudi <sudheer.tatapudi@oracle.com>
2020-06-07 18:40:22 +0530

    added proxy for CORS

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,8 @@ import { TRAIN_SEARCH } from "../constants/action-types";
 export function searchTrain(fromstation,tostation,traveldate) {
     return function(dispatch) {
       return fetch("/from/"+fromstation.fromstation+"/to/"+tostation.tostation+"/date/"+traveldate.traveldate)
-        .then(response => {return response.json()})
+        .then(response => {console.log(response)
+          return response.json()})
         .then(json => {
           dispatch({ type: TRAIN_SEARCH, payload: json });
         });
