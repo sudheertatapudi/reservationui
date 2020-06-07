8b06bbcc0da47e09e2a50d4332639ed89119f996
sudheertatapudi <sudheer.tatapudi@oracle.com>
2020-06-07 12:23:40 +0530

    Updates

---
+++ precache-manifest.621100b1292ef33a086971d9516c053e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f264521c45545a6ef65d4534ae9b00",
+    "url": "/reservationui/index.html"
+  },
+  {
+    "revision": "bc62deacbb23ae0e5464",
+    "url": "/reservationui/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3603c02e5f4c8fd4e787",
+    "url": "/reservationui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc62deacbb23ae0e5464",
+    "url": "/reservationui/static/js/2.5e82237f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reservationui/static/js/2.5e82237f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3603c02e5f4c8fd4e787",
+    "url": "/reservationui/static/js/main.b6a92bef.chunk.js"
+  },
+  {
+    "revision": "d4178bf7bad89da5fefa",
+    "url": "/reservationui/static/js/runtime-main.c6bfa37b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reservationui/static/media/logo.5d5d9eef.svg"
+  }
+]);
