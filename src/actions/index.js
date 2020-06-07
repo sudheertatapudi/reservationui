8d9e6df4ce7f53c0c311c50684aa7a1610ba9165
sudheertatapudi <sudheer.tatapudi@oracle.com>
2020-06-07 12:56:02 +0530

    reservation first commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,14 @@ import { TRAIN_SEARCH } from "../constants/action-types";
 
 export function searchTrain(fromstation,tostation) {
     return function(dispatch) {
-      return fetch("http://172.17.248.33:8080/trains/from/"+fromstation.fromstation+"/to/"+tostation.tostation+"/date/2020-06-07")
+      return fetch("https://jsonplaceholder.typicode.com/posts")
         .then(response => response.json())
         .then(json => {
           dispatch({ type: TRAIN_SEARCH, payload: json });
         });
     };
   }
+
+  
+
+  //"http://172.17.248.33:8080/trains/from/"+fromstation.fromstation+"/to/"+tostation.tostation+"/date/2020-06-07"
