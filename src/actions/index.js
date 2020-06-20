faa71a3d76e3a0495184786385d475b6b99490c8
sudheertatapudi <sudheer.tatapudi@oracle.com>
2020-06-20 09:28:06 +0530

    removed proxy entry and changed service ip

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import { TRAIN_SEARCH } from "../constants/action-types";
 
 export function searchTrain(fromstation,tostation,traveldate) {
     return function(dispatch) {
-      return fetch(`trains/from/${fromstation.fromstation}/to/${tostation.tostation}/date/${traveldate.traveldate}`)
+      return fetch(`https://172.17.157.145:8080/trains/from/${fromstation.fromstation}/to/${tostation.tostation}/date/${traveldate.traveldate}`)
         .then(response => {console.log(response)
           return response.json()})
         .then(json => {
